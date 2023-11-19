86e9ff8c969d46b25b839eac30f08aad5514f935
Bl00dWolf <steamserver2@gmail.com>
2023-11-19 12:40:50 +0300

    Написание меню и пунктов меню. Кнопки "Передача файлов" и "Настройки" созданы

--- main.py
+++ main.py
@@ -1,31 +1,58 @@
 import tkinter
 import tkinter.messagebox
 import customtkinter
-from cert_gen import create_self_signed_cert
-
-# Настройки темы
-customtkinter.set_appearance_mode("System")  # Темы: system (default), light, dark
-customtkinter.set_default_color_theme("blue")  # Темы тоже, расцветка: blue (default), dark-blue, green
-
-# Главное окно
-app = customtkinter.CTk()  # создаем главное окно
-app.geometry("400x240")  # Размер окна
-app.resizable(False, False)  # Нельзя менять размеры окна
-app.eval('tk::PlaceWindow . center')  # Размещаем главное окно по центру экрана (на самом деле не совсем)
+import settings
 
 
-def button_function():
-    tkinter.messagebox.showinfo('Test', message='Test2')
+def main():
+    # список всех созданных кнопок
+    buttons_list = []
+
+    # отключить текущую кнопку, включить все другие в навигационном меню
+    def disable_button(button):
+        cur_fg_color = button.cget('fg_color')
+        # возвращаем все кнопки в исходное, рабочее состояние
+        [(bt.configure(state=customtkinter.NORMAL, fg_color=cur_fg_color)) for bt in buttons_list]
+        # отключаем текущую нажатую кнопку и подсвечиваем, что выбран этот пункт меню
+        button.configure(state=customtkinter.DISABLED, fg_color='#3b9e0d', text_color_disabled='#f6f5fa')
+
+    def settings_button_pressed(button):
+        disable_button(button)
+
+    # Настройки темы
+    customtkinter.set_appearance_mode("System")  # Темы: system (default), light, dark
+    customtkinter.set_default_color_theme("blue")  # Темы тоже, расцветка: blue (default), dark-blue, green
+
+    # Главное окно
+    app = customtkinter.CTk()  # создаем главное окно
+    app.title('Simple File Transfer')  # Заголовок окна
+    app.iconbitmap('icon.ico')
+    app.geometry("800x500")  # Размер окна
+    app.resizable(False, False)  # Нельзя менять размеры окна
+    app.eval('tk::PlaceWindow . center')  # Размещаем главное окно по центру экрана (на самом деле не совсем)
+
+    # Левая навигационная панель, она же главная
+    navigation_panel = customtkinter.CTkFrame(app)
+    navigation_panel.pack(side=tkinter.LEFT)
+    navigation_panel.configure(width=200, height=500)
+
+    # Пункт меню, кнопка "Передача файлов"
+    files_pg_button = customtkinter.CTkButton(master=navigation_panel, text='Передача файлов',
+                                              command=lambda: settings_button_pressed(files_pg_button),
+                                              font=('Bold', 20), bg_color='transparent')
+    files_pg_button.place(relx=0.5, rely=0.1, anchor=customtkinter.CENTER)
+
+    # Пункт меню, кнопка "Передача файлов"
+    settings_pg_button = customtkinter.CTkButton(master=navigation_panel, text='Настройки',
+                                                 command=lambda: settings_button_pressed(settings_pg_button),
+                                                 font=('Bold', 20), bg_color='transparent')
+    settings_pg_button.place(relx=0.5, rely=0.2, anchor=customtkinter.CENTER)
+
+    buttons_list.extend([files_pg_button, settings_pg_button])
+
+    # Так надо
+    app.mainloop()
 
 
-# Кнопка
-button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
-button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
-
-# Левая навигационная панель, она же главная
-navigation_panel = customtkinter.CTkFrame(app)
-navigation_panel.pack(side=tkinter.LEFT)
-navigation_panel.configure(width=100, height=240)
-
-# Так надо =)
-app.mainloop()
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,7 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+
+def button_function():
+    tkinter.messagebox.showinfo('Test', message='Test2')
