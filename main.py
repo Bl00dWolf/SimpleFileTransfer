8de33d79acb113bf6858bb4e1c600b10c8ee3e18
Bl00dWolf <steamserver2@gmail.com>
2024-10-29 19:27:38 +0300

    Настройки Формирование настроек

--- main.py
+++ main.py
@@ -1,17 +1,43 @@
+import json
+
 import flet as ft
-from flet_core import RouteChangeEvent
+import os
+
+settings = {'app_theme': 'dark', 'app_height': 700, 'app_width': 500}
+
+
+def settings_file() -> None:
+    new_params = False
+    if os.path.exists('settings.json'):
+        with open('settings.json', 'r', encoding='utf-8') as file:
+            global settings
+            settings_temp = json.load(file)
+
+            for key, value in settings.items():
+                if key not in settings_temp.keys():
+                    settings_temp[key] = value
+                    new_params = True
+
+            settings = settings_temp
+
+        if new_params:
+            with open('settings.json', 'w', encoding='utf-8') as file:
+                json.dump(settings_temp, file, indent=4)
+
+    else:
+        with open('settings.json', 'w', encoding='utf-8') as file:
+            json.dump(settings, file, ensure_ascii=True, indent=4)
 
 
 def main(page: ft.Page) -> None:
     page.title = 'Simple File Transfer'
     page.window.alignment = ft.Alignment(0, 0)
-    page.window.height = 700
-    page.window.width = 500
+    page.window.height = settings['app_height']
+    page.window.width = settings['app_width']
 
-    page.theme_mode = 'dark'
+    page.theme_mode = settings['app_theme']
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-
 
     page.navigation_bar = ft.NavigationBar(
         destinations=[
@@ -26,4 +52,6 @@ def main(page: ft.Page) -> None:
     )
     page.add(ft.Text("Body!"))
 
+
+settings_file()
 ft.app(target=main)
--- settings.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-import tkinter.messagebox
-
-def button_function():
-    tkinter.messagebox.showinfo('Test', message='Test2')
